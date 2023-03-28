9687431fdb0ddc7b6b73a31081854c320f58f2eb
Kaspervdlaan <kaspervdlaan@gmail.com>
2023-03-28 22:37:37 +0200

    block service and controller done

--- src/main/java/com/example/TopTracker/controllers/BlockController.java
+++ src/main/java/com/example/TopTracker/controllers/BlockController.java
@@ -1,16 +1,15 @@
 package com.example.TopTracker.controllers;
 
 import com.example.TopTracker.dto.BlockDto;
+import com.example.TopTracker.dto.UserDto;
 import com.example.TopTracker.models.Block;
 import com.example.TopTracker.service.BlockService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("blocks")
@@ -28,5 +27,35 @@ public class BlockController {
                 .buildAndExpand(blockDto).toUri();
 
         return ResponseEntity.created(uri).body(b);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BlockDto>> getBlocks() {
+        List<BlockDto> blockDtos = blockService.getAllBlocks();
+
+        return ResponseEntity.ok().body(blockDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BlockDto> getBlockById(@PathVariable Long id) {
+        BlockDto blockDto = blockService.getUserById(id);
+
+        return ResponseEntity.ok(blockDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateBlockById(@PathVariable Long id, @RequestBody BlockDto blockDto) {
+        BlockDto blockDTO = blockService.updateBlock(id, blockDto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .buildAndExpand(blockDTO).toUri();
+
+        return ResponseEntity.created(uri).body(blockDTO);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteBlockById(@PathVariable Long id) {
+        blockService.deleteBlockById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/TopTracker/service/BlockService.java
+++ src/main/java/com/example/TopTracker/service/BlockService.java
@@ -2,30 +2,78 @@ package com.example.TopTracker.service;
 
 import com.example.TopTracker.dto.AreaDto;
 import com.example.TopTracker.dto.BlockDto;
+import com.example.TopTracker.dto.UserDto;
+import com.example.TopTracker.exeption.ResourceNotFoundException;
 import com.example.TopTracker.models.Area;
 import com.example.TopTracker.models.Block;
+import com.example.TopTracker.models.User;
 import com.example.TopTracker.repository.BlockRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BlockService {
-    private final BlockRepository blockRepo;
+    private final BlockRepository blockRepository;
 
     public BlockService(BlockRepository blockRepo) {
-        this.blockRepo = blockRepo;
+        this.blockRepository = blockRepo;
     }
 
     public Block createBlock(BlockDto blockDto) {
         Block b = new Block();
+
         b.setBlockName(blockDto.blockName);
         b.setStoneType(blockDto.stoneType);
 
-        blockRepo.save(b);
-
-        b.getId();
-
-        blockRepo.save(b);
+        blockRepository.save(b);
+        blockDto.id = b.getId();
+        blockRepository.save(b);
 
         return b;
+    }
+
+    public List<BlockDto> getAllBlocks() {
+        List<BlockDto> blocks = new ArrayList<>();
+        List<Block> blockList = blockRepository.findAll();
+        BlockDto blockDto = new BlockDto();
+        for (Block b : blockList) {
+
+            blockDto.id = b.getId();
+            blockDto.blockName = b.getBlockName();
+            blockDto.stoneType = b.getStoneType();
+
+            blocks.add(blockDto);
+        }
+
+        return blocks;
+    }
+
+    public BlockDto getUserById(Long id) {
+        Block b = blockRepository.findById(id).orElseThrow(() -> new RuntimeException("Block not found"));
+        BlockDto blockDto = new BlockDto();
+
+        blockDto.id = b.getId();
+        blockDto.blockName = b.getBlockName();
+        blockDto.stoneType = b.getStoneType();
+
+        return blockDto;
+    }
+
+    public BlockDto updateBlock(Long id , BlockDto blockDto){
+        Block b = blockRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Block not found"));
+
+        b.setBlockName(blockDto.blockName);
+        b.setStoneType(blockDto.stoneType);
+
+        blockRepository.save(b);
+
+        return blockDto;
+    }
+
+    public void deleteBlockById(Long id) {
+        Block b = blockRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Block not found"));
+        blockRepository.delete(b);
     }
 }
